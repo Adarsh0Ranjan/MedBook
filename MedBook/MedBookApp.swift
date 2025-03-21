7a3e215919b75d8daaed9d75dd87cbbb13194985
Adarsh Ranjan <adarsh.ranjan@roro.io>
2025-03-21 10:00:54 +0530

    Initial Commit

---
+++ MedBook/MedBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedBookApp.swift
+//  MedBook
+//
+//  Created by Adarsh Ranjan on 21/03/25.
+//
+
+import SwiftUI
+
+@main
+struct MedBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
